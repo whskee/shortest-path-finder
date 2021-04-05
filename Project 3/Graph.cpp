786f84963bc0b632c7dfda70b7ca9d2b63849207
Chinmaya Bidkar <chinmayabidkar@hotmail.com>
2021-04-05 12:01:12 -0700

    added functionality to add edges

--- Project 3/Graph.cpp
+++ Project 3/Graph.cpp
@@ -4,39 +4,41 @@
 using namespace std;
 
 // function to initialize the graph
-Graph* Initialize(int V, int E) {
+Graph* Initialize(int n, int m) {
     Graph* graph = new Graph;
-    graph->V = V;
-    graph->E = E;
-    graph->Adj = new AdjList[V + 1]; // Adj[u] contains all vertices adjacent to u
+    graph->vertices = n;
+    //graph->Adj = new Node[n + 1];            // Adj[u] contains all vertices adjacent to u
+    graph->Adj = (Node**) calloc(n+1, sizeof(Node*));
+    graph->V = new Vertex[n + 1];
     return graph;
 }
 
 // function to add an edge
 void addEdge(Graph *graph, int src, int dest) {
-    AdjListNode *node = new AdjListNode;
-    node->dest = dest;
-    node->next = NULL;
-    node->next = graph->Adj[src].head;
+    Node *node = new Node;
+    node->v = dest;
+    node->next = graph->Adj[src];
+    graph->Adj[src] = node;
 }
 
 // function to add an edge
 void addEdge(Graph *graph, int src, int dest, int weight) {
-    AdjListNode *node = new AdjListNode;
-    node->dest = dest;
-    node->weight = weight;
-    node->next = graph->Adj[src].head;
-    graph->Adj[src].head = node;
-    cout << "added<" << src << ", " << dest << ", " << weight << ">\n";
-    cout << graph->Adj[src].head->dest << "\n";
+    Node *node = new Node;
+    node->v = dest;
+    node->w = weight;
+    node->next = graph->Adj[src];
+    graph->Adj[src] = node;
+    
+//    cout << "added<" << src << ", " << dest << ", " << weight << ">\n";
+//    cout << graph->Adj[src]->w << "\n";
 }
 
 void printGraph(Graph *graph) {
-    for (int x = 1; x <= graph->V; x++) {
-        AdjListNode *temp = graph->Adj[x].head;
+    for (int x = 1; x <= graph->vertices; x++) {
+        Node *temp = graph->Adj[x];
         cout << x << ": ";
-        while (temp->next != NULL) {
-            cout << temp->dest << "-->";
+        while (temp != NULL) {
+            cout << " --> " << temp->v;
             temp = temp->next;
         }
         cout << "\n";
--- Project 3/Graph.h
+++ Project 3/Graph.h
@@ -1,27 +1,33 @@
 #pragma once
 
 // struct to represent the vertices
-struct AdjListNode {
-    int dest;
-    int weight;
-    int dsc, fin;
+typedef struct Vertex {
+    int pi;                 // parent index
     char color;
-    AdjListNode *next;
-    AdjListNode *parent;
-};
+    int dsc, fin;
+    float d;                // distance
+    int pos;                // index in heap array
+}Vertex;
+
 
 // struct to represent the edges of the graph
-struct AdjList {
-    AdjListNode *head;
-};
+typedef struct Node {
+    int v;                  // stores vertex id
+    float w;                // weight
+    Node *next;             // pointer to next element
+}Node;
+
 
 // class to represent a graph
 struct Graph {
-    int V, E;
-    AdjList *Adj; // array of linked lists
+    Node **Adj;             // array of linked lists
+    Vertex *V;
+    int vertices;
 };
 
-Graph* Initialize(int V, int E);
+
+// functions to be implemented in Graph.cpp
+Graph* Initialize(int n, int m);
 void addEdge(Graph *graph, int src, int dest);
 void addEdge(Graph *graph, int src, int dest, int weight);
 void performDijkstra(Graph *graph, int weight, int src);
