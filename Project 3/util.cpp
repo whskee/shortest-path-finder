8825b4b473d83f4a63a08eac98bb9f13afabe563
Chinmaya Bidkar <chinmayabidkar@hotmail.com>
2021-04-17 11:31:34 -0700

    can read user input for this project

--- Project 3/util.cpp
+++ Project 3/util.cpp
@@ -5,29 +5,47 @@
 using namespace std;
 
 // get user input and evaluate it
-string nextCommand(string &strInput, int &src, int &dest) {
+string nextCommand(string &strInput, int &src, int &dest, int &flag) {
 
     string input;
+    string path;
 
     while (1) {
-        // get user command and convert it to upper case
+        // get user command and convert it to upper case(remove TBD)
         cin >> input;
 
         if (input == " " || input == "\t" || input == "\n") {
             continue;
         }
 
-        // IMPORTANT: change this!
-        if (input == "write" || input == "stop") {
-            input = "write path";
+        if (input == "write") {
+            cin >> path;
+            if (path != "path") {
+                cout << "Error: invalid write path query\n";
+                break;
+            }
+            input = input + " " + path;
+            cin >> src;
+            cin >> dest;
+            cout << "Query: write path " << src << " " << dest << "\n";
+            break;
+        }
+
+        if (input == "stop") {
+            input = "stop";
+            cout << "Query: stop"
+                 << "\n";
             break;
         }
 
         if (input == "find") {
             cin >> src;
             cin >> dest;
+            cin >> flag;
+            cout << "Query: find " << src << " " << dest << " " << flag << "\n";
             break;
         }
+        break;
     }
 
     return input;
